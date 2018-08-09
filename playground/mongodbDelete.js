d8e582313d1c1091320627ef6523603eec7ae5b9
Samuel Brown <sbrown97@unm.edu>
2018-08-09 16:30:29 -0600

    see for usages of mongodb delete functionality

--- playground/mongodbDelete.js
+++ playground/mongodbDelete.js
@@ -7,9 +7,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
   console.log('Connected to MongoDB server!');
   const db = client.db('TodoApp');
 
-  db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+  // db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
+  //   console.log(result);
+  // })
+
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //   console.log(result);
+    // })
+
+  db.collection('User').findOneAndDelete({ _id: new ObjectID("5b6cb76e0c4a9931829a25b6")}).then((result) => {
     console.log(result);
   });
+
+  // db.collection('User').deleteMany({name: 'Sam'}).then((result) => {
+  //   console.log(result);
+  // });
 
   // client.close();
 });
